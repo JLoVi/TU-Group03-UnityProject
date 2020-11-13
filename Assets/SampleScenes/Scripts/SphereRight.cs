33606317800f2dbbdc049467fcf35ca70b505018
Francesco <francesco.doninelli@gmail.com>
2020-11-13 16:11:10 +0100

    13.11_afternoon

--- Assets/SampleScenes/Scripts/SphereRight.cs
+++ Assets/SampleScenes/Scripts/SphereRight.cs
@@ -1,22 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
 
-public class WaterRaise : MonoBehaviour
-
+public class sphereRight : MonoBehaviour
 {
-    public float distancePerSecond;
-    public string islandName;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("This is " + islandName);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, distancePerSecond * Time.deltaTime, 0);
+        
     }
 }
