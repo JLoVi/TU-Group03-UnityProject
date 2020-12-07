05e7db533cb40df537a0a0bd3cae54a938385c9a
Francesco <francesco.doninelli@gmail.com>
2020-12-07 12:54:31 +0100

    f

---
+++ Assets/Scripts/ConsumeElevate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsumeElevate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        //create a copy of Step (Parent Game Object with Children) Transform it 0.2 into y.
+        //spend one crystal
+    }
+}
