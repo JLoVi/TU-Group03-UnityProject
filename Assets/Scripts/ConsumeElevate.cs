7236967c1bfc5632e28f7159ab2d3cb2a566ebdc
jelena viskovic <jelena.viskovic@gmail.com>
2020-12-07 12:39:31 +0000

    update

--- Assets/Scripts/ConsumeElevate.cs
+++ Assets/Scripts/ConsumeElevate.cs
@@ -1,9 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class ConsumeElevate : MonoBehaviour
 {
+    
+
+    public Text crystalAmountDisplay;
+
+    public GameObject step;
+
+    public int elevationScale;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +31,16 @@ public class ConsumeElevate : MonoBehaviour
     void OnMouseDown()
     {
         //create a copy of Step (Parent Game Object with Children) Transform it 0.2 into y.
+
+        Vector3 newposition = new Vector3(step.transform.position.x, step.transform.position.y + elevationScale, step.transform.position.z);
+
+        Instantiate(step, newposition, Quaternion.identity);
+
         //spend one crystal
+        ScoreManager.crystalAmount--;
+
+        crystalAmountDisplay.text = ScoreManager.crystalAmount.ToString();
+
+
     }
 }
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    public static int crystalAmount;
+
+    public Text crystalAmountDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        crystalAmount = 5;
+        crystalAmountDisplay.text = crystalAmount.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
