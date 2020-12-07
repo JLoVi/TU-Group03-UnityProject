ec78576e9f9d910c44497a568e7fc0bed3022307
Francesco <francesco.doninelli@gmail.com>
2020-12-07 22:51:08 +0100

    polish 00

---
+++ Assets/Scripts/DestroyCrystal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DestroyCrystal : MonoBehaviour
+{
+    public Text crystalAmountDisplay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+
+        gameObject.SetActive(false);
+
+        ScoreManager.crystalAmount++;
+
+        crystalAmountDisplay.text = ScoreManager.crystalAmount.ToString();
+    }
+}
